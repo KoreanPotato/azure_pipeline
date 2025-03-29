2fc3caa53b511a3f358e7ae2141a716a6e7d8291
SergeyLim <limsergeystudy@gmail.com>
2025-03-30 00:43:34 +0100

    f 7

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,7 +27,6 @@ provider "azurerm" {
   features {}
 
   subscription_id = var.subscription_id
-  subscription_id = var.subscription_id
   client_id       = var.client_id
   client_secret   = var.client_secret
   tenant_id       = var.tenant_id
